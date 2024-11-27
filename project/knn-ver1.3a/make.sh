b5b779f541cfb7c9b356ad45c5ae5f3b02f8ce4b
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-11-27 14:17:12 +0000

    debug makefile

--- project/knn-ver1.3a/make.sh
+++ project/knn-ver1.3a/make.sh
@@ -1,28 +1,28 @@
 #A1a (-D DT=2 to compile considering float data types)
-gcc -Wall -std=gnu99 -lm -c utils.c timer.c io.c
-gcc -D K=3 -Wall -std=gnu99 -lm -c knn.c
-gcc -D K=3 -Wall -std=gnu99 -lm main.c utils.o timer.o knn.o io.o -o knn
+gcc -Wall -std=gnu99 -c utils.c timer.c io.c -lm
+gcc -D DT=1 -D K=3 -Wall -std=gnu99 -c knn.c -lm
+gcc -D DT=1 -D K=3 -Wall -std=gnu99 main.c knn.o utils.o timer.o io.o -o knn -lm
 
 # A1b (-D DT=2 to compile considering float data types)
-#gcc -Wall -std=gnu99 -lm -c utils.c timer.c io.c
-#gcc -D K=20 -Wall -std=gnu99 -lm -c knn.c
-#gcc -D K=20 -Wall -std=gnu99 -lm main.c utils.o timer.o knn.o io.o -o knn
+#gcc -Wall -std=gnu99 -c utils.c timer.c io.c
+#gcc -D K=20 -Wall -std=gnu99 -c knn.c -lm
+#gcc -D K=20 -Wall -std=gnu99 main.c utils.o timer.o knn.o io.o -o knn -lm
 
 # A1c (-D DT=2 to compile considering float data types)
-#gcc -Wall -std=gnu99 -lm -c utils.c timer.c io.c
-#gcc -D K=3 -Wall -std=gnu99 -lm -c knn.c
-#gcc -D K=3 -Wall -std=gnu99 -lm main.c utils.o timer.o knn.o io.o -o knn
+#gcc -Wall -std=gnu99 -c utils.c timer.c io.c -lm
+#gcc -D K=3 -Wall -std=gnu99 -c knn.c -lm
+#gcc -D K=3 -Wall -std=gnu99 main.c utils.o timer.o knn.o io.o -o knn -lm
 
 # A2: (-D DT=2 to compile considering float data types)
-#gcc -Wall -std=gnu99 -lm -c utils.c timer.c io.c
+#gcc -Wall -std=gnu99 -c utils.c timer.c io.c -lm
 #gcc -Wall -D K=20 -D NUM_TRAINING_SAMPLES=8004 -D NUM_TESTING_SAMPLES=1996 -D NUM_FEATURES=100
-#-D NUM_CLASSES=8 -std=gnu99 -lm -c knn.c
+#-D NUM_CLASSES=8 -std=gnu99 -c knn.c -lm
 #gcc -Wall -D K=20 -D NUM_TRAINING_SAMPLES=8004 -D NUM_TESTING_SAMPLES=1996 -D NUM_FEATURES=100
-#-D NUM_CLASSES=8 -std=gnu99 -lm main.c utils.o timer.o knn.o io.o -o knn
+#-D NUM_CLASSES=8 -std=gnu99 main.c utils.o timer.o knn.o io.o -o knn -lm
 
 # A3:
-#gcc -Wall -std=gnu99 -lm -c utils.c timer.c io.c
+#gcc -Wall -std=gnu99 -c utils.c timer.c io.c -lm
 #gcc -Wall -D K=20 -D NUM_TRAINING_SAMPLES=40002 -D NUM_TESTING_SAMPLES=9998 -D
-#NUM_FEATURES=100 -D NUM_CLASSES=8 -std=gnu99 -lm -c knn.c
+#NUM_FEATURES=100 -D NUM_CLASSES=8 -std=gnu99 -c knn.c -lm
 #gcc -Wall -D K=20 -D NUM_TRAINING_SAMPLES=40002 -D NUM_TESTING_SAMPLES=9998 -D
-#NUM_FEATURES=100 -D NUM_CLASSES=8 -std=gnu99 -lm main.c utils.o timer.o knn.o io.o -o knn
+#NUM_FEATURES=100 -D NUM_CLASSES=8 -std=gnu99 main.c utils.o timer.o knn.o io.o -o knn -lm
