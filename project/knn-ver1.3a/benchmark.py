d2b73a85882e0d228f94af221c5e8a5bca82b0dd
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-12-03 15:01:24 +0000

    Some plots

--- project/knn-ver1.3a/benchmark.py
+++ project/knn-ver1.3a/benchmark.py
@@ -1,5 +1,8 @@
 import subprocess
 import shutil
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import os
 
 def runCompiler(name):
@@ -18,6 +21,9 @@ def runGeneric(command):
     )
 
 def ExecuteAndParse(d, numIterations, scenario):
+
+
+    times = []
     for i in range(numIterations+1):
 
         if i == 0:
@@ -38,22 +44,40 @@ def ExecuteAndParse(d, numIterations, scenario):
         with open("profiling/profile.output", "r", encoding="utf-8") as file:
             lines = file.readlines()
             start = False
+
+            prev_line = None
             for line in lines[4:]:
                 stats = line.strip().split()
 
                 if not stats:
+                    os.remove("gmon.out")
+                    times.append(prev_line[1])
                     break
+                
                 if stats[0] == 'time':
                     start = True
+                    continue
                 if not start:
                     continue
                 
-                print(stats)
-                d["percentage"].append(stats[0])
-                d["time"].append(stats[2])
-                d["calls"].append(stats[3])
-                d["name"].append(stats[len(stats)-1])
-    os.remove("gmon.out")
+                try:
+                    d["percentage"].append(float(stats[0]))
+                except ValueError:
+                    d["percentage"].append(0)
+                try:
+                    d["time"].append(float(stats[2]))
+                except ValueError:
+                    d["time"].append(0)
+                try:
+                    d["calls"].append(float(stats[3]))
+                except ValueError:
+                    d["calls"].append(0)
+                try:
+                    d["name"].append(stats[len(stats)-1])
+                except ValueError:
+                    d["name"].append("null")
+                prev_line = stats
+    return times
 
 def ImportData(scenario):
     if os.path.exists("src/data/"):
@@ -64,6 +88,49 @@ def ImportData(scenario):
     runGeneric(f"cp 'scenario-{scenario}/params.h' 'src/params.h'")
 
 
+def DrawGraph(data, scenario):
+
+    f, ax1 = plt.subplots(figsize=(20, 10))
+    df = pd.DataFrame(data)
+    sns.set_theme(style="white", context="talk")
+    sns.barplot(df,x='name', y='time', hue='name', palette="light:m_r", edgecolor=".3", linewidth=.5, ax=ax1)
+    
+    ax1.axhline(0, color="k", clip_on=False)
+    ax1.set_ylabel('Latency (s)')
+    ax1.set_xlabel(f'Functions on {scenario} K-NN')
+    plt.title('Function Latencies')
+    plt.tight_layout(h_pad=2)
+
+    plt.savefig(f'profiling/graphs/plot_latency_{scenario}.png')
+
+    f, ax2 = plt.subplots(figsize=(20, 10))
+    sns.set_theme(style="white", context="talk")
+    sns.barplot(df,x='name', y='calls', hue='name', palette="light:m_r", edgecolor=".3", linewidth=.5, ax=ax2)
+    
+    ax2.axhline(0, color="k", clip_on=False)
+    ax2.set_ylabel('Calls Count')
+    ax2.set_xlabel(f'Functions on {scenario} K-NN')
+    plt.title('Function Calls')
+    plt.tight_layout(h_pad=2)
+
+    plt.savefig(f'profiling/graphs/plot_calls_{scenario}.png')
+
+def DrawGraphFinal(data):
+
+    f, ax1 = plt.subplots(figsize=(20, 10))
+    df = pd.DataFrame(data)
+    sns.set_theme(style="white", context="talk")
+    sns.barplot(df,x='scenario', y='time', hue='scenario', palette="light:m_r", edgecolor=".3", linewidth=.5, ax=ax1)
+    
+    ax1.axhline(0, color="k", clip_on=False)
+    ax1.set_ylabel('Latency (s)')
+    ax1.set_xlabel(f'All Scenarios K-NN')
+    plt.title('Function Latencies')
+    plt.tight_layout(h_pad=2)
+
+    plt.savefig(f'profiling/graphs/plot_total.png')
+    return
+
 
 
 def CompileAndRun(numIterations):
@@ -72,6 +139,11 @@ def CompileAndRun(numIterations):
         os.remove("benchmark_result.output")
 
     with open("benchmark_result.output", "w", encoding="utf-8") as output:
+
+        runs = {
+            "time": [],
+            "scenario": []
+        }
 
         '''
         print("SIMPLE SCENARIO")
@@ -110,11 +182,15 @@ def CompileAndRun(numIterations):
             "name": []
         }
         print("Executing and Parsing... ")
-        ExecuteAndParse(d, numIterations, "k3")
+        total_time = ExecuteAndParse(d, numIterations, "k3")
         print("Done!")
         print("Produing graphs... ")
-        # graphs here
+        DrawGraph(d, "k3")
         print("Done!\n")
+
+        for i in range(len(total_time)):
+            runs["scenario"].append('k3')
+            runs["time"].append(total_time[i])
         
         print("K20 SCENARIO")
         print("Import data... ")
@@ -130,13 +206,15 @@ def CompileAndRun(numIterations):
             "name": []
         }
         print("Executing and Parsing... ")
-        ExecuteAndParse(d, numIterations, "k20")
+        total_time = ExecuteAndParse(d, numIterations, "k20")
         print("Done!")
         print("Produing graphs... ")
-        # graphs here
+        DrawGraph(d, "k20")
         print("Done!\n")
 
-    
+        for i in range(len(total_time)):
+            runs["scenario"].append('k20')
+            runs["time"].append(total_time[i])
 
         print("g100x8x5000 SCENARIO")
         print("Import data... ")
@@ -152,11 +230,15 @@ def CompileAndRun(numIterations):
             "name": []
         }
         print("Executing and Parsing... ")
-        ExecuteAndParse(d, numIterations, "g100x8x5000")
+        total_time = ExecuteAndParse(d, numIterations, "g100x8x5000")
         print("Done!")
         print("Produing graphs... ")
-        # graphs here
+        DrawGraph(d, "g100x8x5000")
         print("Done!\n")
+
+        for i in range(len(total_time)):
+            runs["scenario"].append('g100x8x5000')
+            runs["time"].append(total_time[i])
 
         print("g100x8x1000 SCENARIO")
         print("Import data... ")
@@ -172,11 +254,17 @@ def CompileAndRun(numIterations):
             "name": []
         }
         print("Executing and Parsing... ")
-        ExecuteAndParse(d, numIterations, "g100x8x1000")
+        total_time = ExecuteAndParse(d, numIterations, "g100x8x1000")
         print("Done!")
         print("Produing graphs... ")
-        # graphs here
+        DrawGraph(d, "g100x8x1000")
         print("Done!\n")
+
+        for i in range(len(total_time)):
+            runs["scenario"].append('g100x8x1000')
+            runs["time"].append(total_time[i])
+
+        DrawGraphFinal(runs)
 
 if __name__ == "__main__":
     CompileAndRun(10)
